e4a49f25daf769a8578347332ff78b32b3f118bd
Mark Nyon <mark@grandkru.com>
2013-07-22 14:06:19 -0400

    Updated path

--- lib/refinery/locations/engine.rb
+++ lib/refinery/locations/engine.rb
@@ -9,7 +9,7 @@ module Refinery
         Refinery::Plugin.register do |plugin|
           plugin.pathname = root
           plugin.name = "refinerycms_locations"
-          plugin.url = proc { "/refinery/admin/locations" }
+          plugin.url = proc {  Refinery::Core::Engine.routes.url_helpers.locations_admin_location_path }
           plugin.activity = { :class_name => :'refinery/locations/location' }
         end
       end
