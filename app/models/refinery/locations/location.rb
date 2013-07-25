6c93cf3950ec0b55d2d254f1d5d2d4d6486e0188
Mark Nyon <mark@grandkru.com>
2013-07-25 12:09:19 -0400

    Updated location view

--- app/models/refinery/locations/location.rb
+++ app/models/refinery/locations/location.rb
@@ -1,7 +1,7 @@
 module Refinery
 	module Locations
 		class Location < ActiveRecord::Base
-		  #acts_as_indexed :fields => [:name, :address, :phone, :hours, :longitude, :latitude]
+		  acts_as_indexed :fields => [:name, :address, :phone, :hours, :longitude, :latitude]
 		   self.table_name = 'refinery_locations'
 		   validates_presence_of :name
 		  validates_uniqueness_of :name
