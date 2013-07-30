a26b016ffca94ae90d3675460b10ac0729a68cb9
Mark Nyon <mark@grandkru.com>
2013-07-30 14:12:23 -0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,35 @@
-Refinery::Core::Engine.routes.draw do
-  namespace :locations do
-  	resources :locations
-  end
-
-  namespace :locations, :path => '' do
-    namespace :admin, :path => 'refinery/locations' do
-    	resources :locations
-    end
-  end
-
-  namespace :locations, :path => '' do
-    namespace :admin, :path => 'refinery/locations' do
-    	resources :imports
-    end
-  end
-
+Rails.application.routes.draw do
+  mount Refinery::Core::Engine, :at => "/"
 end
+
+Refinery::Core::Engine.routes.append do
+
+  # Frontend routes
+  namespace :locations do
+    resources :locations, :path => '', :only => [:index, :show]
+  end
+
+  # Admin routes
+  namespace :locations, :path => '' do
+    namespace :admin, :path => 'refinery/locations' do
+      resources :locations, :except => :show do
+        collection do
+          post :update_positions
+        end
+      end
+    end
+  end
+
+
+  # Admin routes
+  namespace :locations, :path => '' do
+    namespace :admin, :path => 'refinery/locations' do
+      resources :imports, :except => :show do
+        collection do
+          post :update_positions
+        end
+      end
+    end
+  end
+end
+
